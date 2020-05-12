172efc34b510195642d886f56f74eacba954623a
zlatoslava <anckamur@gmail.com>
2020-05-12 20:34:43 +0200

    Databinding in SignUpActivity

--- app/src/main/java/com/example/loginpage/ui/SignUpActivity.kt
+++ app/src/main/java/com/example/loginpage/ui/SignUpActivity.kt
@@ -2,12 +2,25 @@ package com.example.loginpage.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.loginpage.R
+import com.example.loginpage.databinding.ActivitySignUpBinding
+import com.example.loginpage.viewmodels.UserViewModel
+import com.example.loginpage.viewmodels.ViewModelFactory
 
 class SignUpActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var userVM: UserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+
+        userVM =
+            ViewModelProvider(this, ViewModelFactory(application)).get(UserViewModel::class.java)
+        binding.userVM = userVM
+        binding.executePendingBindings() //To apply binding immediately (before the view state is restored)
     }
 }
