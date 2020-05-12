46449948a8eb488b3acdefdbc0eaf3a6cc51f67f
zlatoslava <anckamur@gmail.com>
2020-05-12 20:12:42 +0200

    Welcome user in MainActivity

--- app/src/main/java/com/example/loginpage/ui/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/ui/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.loginpage.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import com.example.loginpage.R
+import com.example.loginpage.util.PREF_USERNAME
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(application)
+        val username = preferences.getString(PREF_USERNAME, "")
+        text_main_activity.text = "Welcome $username!"
     }
 }
